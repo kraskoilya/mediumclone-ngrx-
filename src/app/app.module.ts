86ccd68a88049ee3954ce6840dac09f96ece122d
kokokowka <i.krasko@naveksoft.com>
2020-10-07 22:30:44 +0300

    add authorization iterceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { EffectsModule } from '@ngrx/effects';
@@ -8,7 +8,9 @@ import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
+import { AuthInterceptor } from './auth/services/auth-interceptor.service';
 import { TopBarModule } from './shared/modules/top-bar/top-bar.module';
+import { PersistanceService } from './shared/services/persistance.service';
 
 @NgModule({
   declarations: [AppComponent],
@@ -25,7 +27,14 @@ import { TopBarModule } from './shared/modules/top-bar/top-bar.module';
     EffectsModule.forRoot([]),
     TopBarModule,
   ],
-  providers: [],
+  providers: [
+    PersistanceService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
