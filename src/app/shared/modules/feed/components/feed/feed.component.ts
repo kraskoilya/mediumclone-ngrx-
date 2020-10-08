1847f48f0e855873f03ce26e611c0318fd83319b
kokokowka <i.krasko@naveksoft.com>
2020-10-08 14:02:25 +0300

    render feed

--- src/app/shared/modules/feed/components/feed/feed.component.ts
+++ src/app/shared/modules/feed/components/feed/feed.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { getFeedAction } from '../../store/actions/getFeed.action';
+import {
+  errorSelector,
+  feedSelector,
+  isLoadingSelector,
+} from './../../store/selctors';
+import { GetFeedResponceInterface } from './../../types/getFeedResponce.interface';
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
-  styleUrls: ['./feed.component.scss']
+  styleUrls: ['./feed.component.scss'],
 })
 export class FeedComponent implements OnInit {
+  // tslint:disable-next-line:no-input-rename
+  @Input('apiUrl') apiUrlProps: string;
 
-  constructor() { }
+  isLoading$: Observable<boolean>;
+  error$: Observable<string | null>;
+  feed$: Observable<GetFeedResponceInterface | null>;
+
+  constructor(private store: Store) {}
 
   ngOnInit(): void {
+    this.fetchData();
+    this.initializeValues();
   }
 
+  private initializeValues(): void {
+    this.isLoading$ = this.store.pipe(select(isLoadingSelector));
+    this.error$ = this.store.pipe(select(errorSelector));
+    this.feed$ = this.store.pipe(select(feedSelector));
+  }
+  private fetchData(): void {
+    this.store.dispatch(getFeedAction({ url: this.apiUrlProps }));
+  }
 }
--- src/app/shared/modules/feed/feed.module.ts
+++ src/app/shared/modules/feed/feed.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { FeedComponent } from './components/feed/feed.component';
@@ -13,6 +14,7 @@ import { reducers } from './store/reducers';
     CommonModule,
     EffectsModule.forFeature([GetFeedEffect]),
     StoreModule.forFeature('feed', reducers),
+    RouterModule,
   ],
   exports: [FeedComponent],
   providers: [FeedService],
